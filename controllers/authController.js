55d63e8ba45b73aff9c4f8da460793830ef31a6a
Amal <amalskumr20@gmail.com>
2025-03-03 17:41:02 +0000

    Mar 3, 2025, 11:11 PM

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,25 @@
 const User=require('../models/user')
 exports.login=(req,res)=>{
     const {email,password}=req.body
-    
+
 //check for credentials
     User.findOne({email,password},(err,user)=>{
         if(err||!user)
         {
             return res.status(401).send('Invalid email or password')
         }
+        req.session.userId=user._id;
+        req.session.isLoggedIn=true
+        res.redirect('/Homepage')
+    })
+}
+exports.logout=(req,res)=>{
+    //Destroy session
+    req.session.destroy((err)=>{
+        if(err)
+        {
+            return res.status(500).send('Could not log out')
+        }
+        res.redirect('/UserLogin')
     })
 }
