3f233b2797548a99f91fdb44b716095924e9f61d
Amal <amalskumr20@gmail.com>
2025-03-07 11:24:50 +0000

    Mar 7, 2025, 4:54 PM

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use('/', UserRouter);
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
