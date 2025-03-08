e0369bce603b473006b39f8d1dbb25ba2cdec360
Amal <amalskumr20@gmail.com>
2025-03-08 16:39:14 +0000

    Mar 8, 2025, 10:09 PM

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const UserController=require('../controllers/UserController')
-const authMiddleware=require('../middleware/authMiddleware')
+
 
 router.get('/UserLogin',UserController.userlogin)
 router.post('/userLoginPost',UserController.userLoginPost)
