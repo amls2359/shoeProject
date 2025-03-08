319810c54587c88373ddd96bf5d9b94732ac9bf8
Amal <amalskumr20@gmail.com>
2025-03-08 18:16:56 +0000

    Mar 8, 2025, 11:46 PM

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,27 @@ const express=require('express')
 
 const adminLogin=(req,res)=>res.render('admin')
 
+//adminlogin post
+const adminloginpost = async (req, res) => {
+    
+    try {
+        const admin = {
+            username: "admin",
+            password: "12345"
+        }
+
+        if (req.body.username === admin.username && req.body.password === admin.password) {
+            req.session.admin = admin.username;
+            res.redirect("/admin/dashboard");
+        } else {
+           res.redirect('/admin/adminlogin/?message=InvalidEntry')
+            
+        }
+    } catch (error) {
+        console.log("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
 
 module.exports={
     adminLogin
