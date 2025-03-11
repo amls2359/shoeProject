5e5ddabdf7818ee987248a7818c7d41803362446
Amal <amalskumr20@gmail.com>
2025-03-11 08:41:35 +0000

    Mar 11, 2025, 2:11 PM

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 const adminCollection=require('../models/admin')
-const UserCollection=require('../models/user')
+const UserCollection =require('../models/user')
 const express=require('express')
 
 const adminLogin=(req,res)=>res.render('adminLogin')
