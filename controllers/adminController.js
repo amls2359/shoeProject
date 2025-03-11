a83ad72c70116b1eb96e67300a9fff6ebf436d17
Amal <amalskumr20@gmail.com>
2025-03-11 12:29:42 +0000

    Mar 11, 2025, 5:59 PM

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -20,7 +20,7 @@ const adminloginpost = async (req, res) => {
         if (req.body.username === admin.username && req.body.password === admin.password) {
             res.redirect("/admin/dashboard");
         } else {
-            res.redirect('/admin/adminlogin?error=Invalid username or password');
+            res.redirect('/admin/adminLogin?error=Invalid username or password');
         }
     } catch (error) {
         console.log("Error:", error);
