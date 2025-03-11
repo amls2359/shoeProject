dcf351588770a89a16f73548e936a61e95f3e5d1
Amal <amalskumr20@gmail.com>
2025-03-11 12:39:27 +0000

    Mar 11, 2025, 6:09 PM

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,22 +8,18 @@ const dashboard=(req,res)=>res.render('dashboard')
 //adminlogin post
 const adminloginpost = async (req, res) => {
     try {
-        console.log('before');
-        
         const admin = {
             username: "admin",
             password: "12345"
         };
-        console.log('body');
-        
 
         if (req.body.username === admin.username && req.body.password === admin.password) {
             res.redirect("/admin/dashboard");
         } else {
-            return res.render('/admin/adminLogin', { 
-                errorMessage: 'Please fill in all fields', 
+            return res.render('admin/adminLogin', { 
+                errorMessage: 'Invalid username or password', 
                 successMessage: null 
-              });
+            });
         }
     } catch (error) {
         console.log("Error:", error);
