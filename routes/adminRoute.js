5cfb93b15ae36c8d84f4937ace962ead21fd23ca
Amal <amalskumr20@gmail.com>
2025-03-11 12:24:46 +0000

    Mar 11, 2025, 5:54 PM

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,6 +5,6 @@ const adminController=require('../controllers/adminController')
 router.get('/adminLogin',adminController.adminLogin)
 router.post('/adminloginpost',adminController.adminloginpost)
 
-router.get('/admin/dashboard',adminController.dashboard)
+router.get('/dashboard',adminController.dashboard)
 
 module.exports=router
