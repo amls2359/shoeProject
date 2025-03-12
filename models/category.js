1fc2feb82e62370a6208bc1e5bb80a67313e4f97
Amal <amalskumr20@gmail.com>
2025-03-12 18:07:37 +0000

    Mar 12, 2025, 11:37 PM

---
+++ models/category.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose')
+
+const categorySchema= new mongoose.Schema({
+    category:{type:String},
+    islisted:{type:Boolean,default:false}
+
+})
+module.exports=mongoose.model('category',categorySchema)
