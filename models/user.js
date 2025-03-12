af36a212c4e260365cdffb000554ee7ab61dafe9
Amal <amalskumr20@gmail.com>
2025-03-12 14:19:25 +0000

    Mar 12, 2025, 7:49 PM

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, unique: true },
     password: { type: String },
-    username:{type:String}
+    username:{type:String},
+    isblocked: {type: Boolean,default:false},
 });
 
 
