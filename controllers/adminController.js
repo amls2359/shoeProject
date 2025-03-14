f93c3304125a07fc98b81f64b90df49260109090
Amal <amalskumr20@gmail.com>
2025-03-14 16:18:15 +0000

    Mar 14, 2025, 9:48 PM

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -91,6 +91,7 @@ const categorymanagement= async(req,res)=>{
 const addcategoryget = async (req, res) => {
     try {
         const categories = await Category.find({}).sort({ _id: -1 }); // Fetch all categories sorted by ID in descending order
+        console.log("Fetched categories:", categories); // Debugging statement
         res.render('categorymanagement', { categories: categories });
     } catch (err) {
         console.error("Error fetching categories:", err);
