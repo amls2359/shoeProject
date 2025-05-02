25af11b5a3c892c80b1e727e0c3df5adc2bab9b3
Amal <amalskumr20@gmail.com>
2025-05-02 05:30:39 +0000

    May 2, 2025, 11:00 AM

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,7 +1,22 @@
 const express=require('express')
 const router=express.Router()
 const adminController=require('../controllers/adminController')
-const checkSession=require('../middleware/session')
+
+
+const checkSession =async(req,res,next)=>
+{
+    console.log('Reached session');
+    if(req.session.admin)
+    {
+        console.log('session found');
+        next()
+    }
+    else
+    {
+        res.redirect('/admin/adminLogin')
+    }
+    
+}
 
 
 router.get('/adminLogin',adminController.adminLogin)
@@ -10,14 +25,14 @@ router.post('/adminloginpost',adminController.adminloginpost)
 router.get('/dashboard',checkSession,adminController.dashboard)
 
 router.get('/usermanagement',adminController.usermanagement)
-router.get('/blockuser/:id',adminController.block)
+router.get('/blockuser/:id',checkSession,adminController.block)
 router.get('/unblockuser/:id',adminController.unblock)
 
-router.get('/categorymanagement',adminController.categoryManagement)
-router.get('/addcategory',adminController.addcategoryget)
+router.get('/categorymanagement',checkSession,adminController.categoryManagement)
+router.get('/addcategory',checkSession,adminController.addcategoryget)
 
 router.post('/addCategoryPost',adminController.addCategoryPost)
-router.get('/unListcategory/:id',adminController.UnList)
+router.get('/unListcategory/:id',checkSession,adminController.UnList)
 router.get('/editCategory/:id',adminController.editCategoryget)
 router.post('/editCategory/:id',adminController.editCategorypost)
 
